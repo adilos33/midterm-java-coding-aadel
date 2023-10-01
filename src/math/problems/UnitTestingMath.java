200d45ab993811d02541318a1f4f15218ef53a91
MAC <mac@MBP-de-MAC.lan>
2023-10-01 10:39:32 -0400

    midterm-java-coding-aadel

--- src/math/problems/UnitTestingMath.java
+++ src/math/problems/UnitTestingMath.java
@@ -4,5 +4,23 @@ public class UnitTestingMath {
     public static void main(String[] args) {
         //Apply Unit testing into each classes and methods in this package.
 
+
+        import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+        public class FactorialTest {
+
+            @Test
+            public void testCalculateFactorial()
+            int input = 5;
+                int expectedOutput = 120;
+
+
+                int result = Factorial.calculateFactorial(input);
+
+                assertEquals(expectedOutput, result);
+            }
+        }
+
     }
 }
